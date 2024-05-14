0398b7389f712b9a688ffbae3e8994d3264537f1
Colby Sparks <colby.sparks@zaber.com>
2024-05-14 15:21:16 -0700

    Remove node_modules

--- src/hello.ts
+++ src/hello.ts
@@ -1,9 +1,9 @@
 const ratio = 9.0 / 5.0;
 
-function toFarenheit(input: number) :number {
+export function toFarenheit(input: number) :number {
   return (input * ratio) + 32.0;
 }
 
-function toCelcius(input: number) : number {
+export function toCelsius(input: number) : number {
   return (input - 32.0) / ratio;
 }
