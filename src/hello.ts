f257b15a05ec8143d930807fcd2eff7e248822e4
Colby Sparks <colby.sparks@zaber.com>
2024-05-14 15:08:37 -0700

    Revert "First commit"
    This reverts commit a6fce9a6450903613f4f69fff0b0ed8d3e75f354.

--- src/hello.ts
+++
@@ -1,9 +0,0 @@
-const ratio = 9.0 / 5.0;
-
-function toFarenheit(input: number) :number {
-  return (input * ratio) + 32.0;
-}
-
-function toCelcius(input: number) : number {
-  return (input - 32.0) / ratio;
-}
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-// Compiled using undefined undefined (TypeScript 4.9.5)
-var greeter = function (person) {
-    return "Hello, ".concat(person, "!");
-};
-function testGreeter() {
-    var user = 'Grant';
-    Logger.log(greeter(user));
-}
